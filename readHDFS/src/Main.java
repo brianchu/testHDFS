5c7f25073624d8818d265cb19d2ec62ff2b2579f
Brian Chu <sychu@yahoo-inc.com>
2014-05-15 22:43:56 -0700

    test

---
+++ readHDFS/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by sychu on 5/15/14.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.go();
+    }
+
+    private void go() {
+    }
+
+}
